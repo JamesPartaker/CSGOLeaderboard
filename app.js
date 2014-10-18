0e92e656bb14398dafe32a55b63c9c67ade0b507
James Partaker <twin891@gmail.com>
2014-10-18 16:35:46 -0500

    Send message to browser via socket

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var config = require('./config');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
 
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/views/index.html');
+});
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,8 +26,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -60,15 +62,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-//Watch CS:GO file for updates
-var Tail = require('tail').Tail;
 
-var tail = new Tail(config.csgolog_filename, "\n", {}, false);
 
-tail.on("line", function(data) {
-  console.log(data);
-});
 
-tail.on("error", function(error) {
-  console.log('ERROR: ', error);
-});
