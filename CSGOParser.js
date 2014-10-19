7ee69a4bd6cce96a385fc828cafcfc949d32bd33
bamsy <bamsy23@gmail.com>
2014-10-18 19:57:51 -0500

    add bomb{defused,planted} events

--- CSGOParser.js
+++ CSGOParser.js
@@ -4,21 +4,23 @@ var parser = {};
 //checks the line type using regex
 //and parses based on the line
 parser.parseLine = function(line){
-  //var cleanRegex = /s/\(L\s\+\S\+\s\+\S\+\s\+\S\+\s\+\|\[\S\+\s\+\S\+\s\+\S\+\]\s\+\)//g;
-  var killRegex = /".*?"\s\+killed\s+".*"/;
-  //var bombDefuseRegex = new RegExp('');
-  var bombPlantRegex =  new RegExp('');
+
+  var cleanRegex = /L\s+\S+\s+\S+\s+\S+\s+|\[\S+\s+\S+\s+\S+\]\s+/g;
+  var killRegex = /".*?"\s+killed\s+".*?"/;
+  var bombDefuseRegex = /".*?"\s+triggered\s+"Defused_The_Bomb"/;
+  var bombPlantRegex =  /".*?"\s+triggered\s+"Planted_The_Bomb"/;
+  line = line.replace(cleanRegex, "");
 
   if(killRegex.test(line)){
     return parseKill(line);
-  }else if(bombDefuseRegex.test(line)){
+  }
+  else if(bombDefuseRegex.test(line)){
     return parseBombDefuse(line);
   }else if(bombPlantRegex.test(line)){
     return parseBombPlant(line);
   }else{
     return {};
   }
-
 
 };
 
